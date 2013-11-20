83bac9078060ab0d187ccea352e73115b30cf0ff
Lazar Nikolov <laki6sixx@gmail.com>
2013-11-20 01:18:43 +0100

    Nizi

---
+++ Nizi/OtstranuvajnePovtorniElementiNiza.c
@@ -0,0 +1,46 @@
+﻿/*
+Да се направи функција која има два аргументи: низа од N цели броеви 
+и бројот на елементи N. Од низата да се отстранат сите елементи кои се 
+појавуваат повеќе од еднаш. Низата и нејзината должина се вчитува од СВ. 
+На СИ испечатете ги елементите на новодобиената низа оделени со една белина.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+
+int otstranuvanje(int *a, int n){
+	int i, j, jj, br = 0;
+
+	for (i = 0; i < (n - br); i++){
+		for (j = i + 1; j < (n - br); j++){
+			if (a[i] == a[j]){
+				jj = j;
+				for (j = j; j < (n - br); j++){
+					a[j] = a[j + 1];
+				}
+				br++;
+				j = jj;
+			}
+		}
+	}
+
+	for (i = 0; i < (n - br); i++)
+		printf("%d ", a[i]);
+	
+	return 0;
+}
+
+int main(){
+	int a[MAX], n, i;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	otstranuvanje(a, n);
+
+	system("pause");
+	return 0;
+}
---
+++ Nizi/SortiranjeNiza.c
@@ -0,0 +1,39 @@
+﻿/*
+Да се напише функција која за дадена низа А од N (N < 100) природни броеви
+(низата и бројот N се вчитуваат од СВ) ќе ги сортира нејзините елементи во
+растечки редослед и ќе го врати како резултат најмалиот елемент во низата.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
+int sortMin(int *a, int n) {
+	// vasiot kod ovde
+	int pom, i, j;
+
+	for (i = 0; i < n; i++){
+		for (j = i + 1; j < n; j++){
+			if (a[i] > a[j]){
+				pom = a[i];
+				a[i] = a[j];
+				a[j] = pom;
+			}
+		}
+	}
+	return a[0];
+}
+int main() {
+	int a[MAX], n, i, min;
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	min = sortMin(a, n);
+	for (i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+	printf("\n%d", min);
+
+	system("pause");
+	return 0;
+}
